928e45e6cce644c06d27bd01a31f046de99f8593
Aayush Sahu <aayushsahumk48@gmail.com>
2026-07-18 23:46:05 +0530

    Leetcode 507 : Perfect Number

---
+++ Leetcode/PerfectNumber.cpp
@@ -0,0 +1,17 @@
+//Leetcode 507 : Perfect Number
+
+#include <iostream>
+using namespace std;
+
+bool checkPerfectNumber(int num) 
+{
+    int sum = 0;
+
+    for(int i = 1; i <= num/2; i++)
+    {
+        if(num % i == 0)
+            sum += i;
+    }
+
+    return sum == num;
+}
