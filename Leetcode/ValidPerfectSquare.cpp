77103fbfea4799469ab62a34f6dfe6dbbc0d70af
Aayush Sahu <aayushsahumk48@gmail.com>
2026-07-19 23:11:28 +0530

    Leetcode 367 : Valid Perfect Square

---
+++ Leetcode/ValidPerfectSquare.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+bool isPerfectSquare(int num) 
+{
+    for(long long i = 1; i*i <= num; i++)
+    {
+        if(i*i == num)
+        {
+            return true;
+        }
+    }
+    return false;
+}
