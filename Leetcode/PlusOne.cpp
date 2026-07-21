f6d3e62d9b5773362f2894b68cf35547e7744aea
Aayush Sahu <aayushsahumk48@gmail.com>
2026-07-21 22:17:01 +0530

    Leetcode 66 - Plus One

---
+++ Leetcode/PlusOne.cpp
@@ -0,0 +1,24 @@
+// Leetcode 66 : Plus One
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> plusOne(vector<int>& digits) 
+{
+
+    for (int i = digits.size() - 1; i >= 0; i--) 
+    {
+
+        if (digits[i] < 9) {
+            digits[i]++;
+            return digits;
+        }
+
+        digits[i] = 0;
+    }
+
+    digits.insert(digits.begin(), 1);
+
+    return digits;
+}
