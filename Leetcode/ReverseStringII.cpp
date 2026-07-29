9997b61b53aae568424bab5e9b58b08c07cabe5f
Aayush Sahu <aayushsahumk48@gmail.com>
2026-07-29 23:10:03 +0530

    Reverse String II

---
+++ Leetcode/ReverseStringII.cpp
@@ -0,0 +1,25 @@
+// Leetcode 541 : Reverse String II
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+string reverseStr(string s, int k) 
+{
+
+    for (int i = 0; i < s.length(); i += 2 * k) 
+    {
+
+        int left = i;
+        int right = min(i + k - 1, (int)s.length() - 1);
+
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
+        }
+    }
+
+    return s;
+}
