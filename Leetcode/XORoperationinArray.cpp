1459c98a0ee6913edbd10677b2031176a2aa8119
Aayush Sahu <aayushsahumk48@gmail.com>
2026-08-10 00:04:45 +0530

    Leetcode 1486 : XOR Operation in an Array

---
+++ Leetcode/XORoperationinArray.cpp
@@ -0,0 +1,17 @@
+// Leetcode 1486 : XOR Operation in an Array
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int xorOperation(int n, int start) 
+{
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) 
+    {
+        ans ^= start + 2 * i;
+    }
+
+    return ans;
+}
